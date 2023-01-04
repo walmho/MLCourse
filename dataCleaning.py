970f60c98d9f4127318204f2e1cb1f026a363ad7
Ryan Jones <jones.ryan.t.01@gmail.com>
2023-01-04 14:07:12 -0800

    beginning to follow first python tutorial

---
+++ dataCleaning.py
@@ -0,0 +1,9 @@
+#Import Libraries
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+#Import Dataset
+dataset = pd.read_csv("Machine Learning A-Z (Codes and Datasets)/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Python/Data.csv")
+x = dataset.iloc[:, ]
+y = dataset
