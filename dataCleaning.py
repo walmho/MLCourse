08a4140b45e616ab41f145e6b0bc1de8d8c110cc
Ryan Jones <jones.ryan.t.01@gmail.com>
2023-01-06 14:29:20 -0800

    following tutorial, cleaning data and turning nan values into averages

--- dataCleaning.py
+++ dataCleaning.py
@@ -5,5 +5,15 @@ import numpy as np
 
 #Import Dataset
 dataset = pd.read_csv("Machine Learning A-Z (Codes and Datasets)/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Python/Data.csv")
-x = dataset.iloc[:, ]
-y = dataset
+X = dataset.iloc[:,:-1].values
+Y = dataset.iloc[:, -1].values
+print(f"x: {X}, y: {Y}")
+
+#Taking care of missing dataset
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+#Encoding the Independent Variable
+from sklearn.compose import ColumnTransformer
