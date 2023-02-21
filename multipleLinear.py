a2be0a9926514416891e6049806bbc41a7e15e58
Ryan Jones <jones.ryan.t.01@gmail.com>
2023-02-21 14:58:41 -0800

    finished Multiple Linear regression model for python

--- multipleLinear.py
+++ multipleLinear.py
@@ -4,10 +4,23 @@ import pandas as pd
 
 dataset = pd.read_csv("C:/Users/ryan_/Documents/Programming/repos/MLCourse/Machine Learning A-Z (Codes and Datasets)/Part 2 - Regression/Section 5 - Multiple Linear Regression/Python/50_startups.csv")
 x = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
 
 #Encoding categorical into usable data
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough')
 x = np.array(ct.fit_transform(x))
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = .2, random_state = 0)
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
+np.set_printoptions(precision=2)
+print(np.concatenate((y_pred.reshape(len(y_pred),1),y_test.reshape(len(y_test),1)),1))
+
+
