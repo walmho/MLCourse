50b7c1a9831c2caecf50cece61a8c8a5f5edb253
Ryan Jones <jones.ryan.t.01@gmail.com>
2023-02-27 14:29:35 -0800

    finished multiple linear regression in both Python and R

---
+++ multipleLinear_versionR.R
@@ -0,0 +1,19 @@
+# Multiple Linear Regression
+
+dataset = read.csv('50_Startups.csv')
+
+dataset$State = factor(dataset$State,
+                         levels = c('New York', 'California', 'Florida'),
+                         labels = c(1, 2, 3))
+
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Profit, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+regressor = lm(formula = Profit ~ .,
+               data = training_set)
+
+y_pred = predict(regressor, newdata = test_set)
+
