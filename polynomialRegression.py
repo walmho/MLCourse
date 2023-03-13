3ab68ec5cd5d4390ba2c169a21122219d871427a
Ryan Jones <jones.ryan.t.01@gmail.com>
2023-03-13 14:55:49 -0700

    added basic polynomial regression example

---
+++ polynomialRegression.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  9 14:16:59 2023
+
+@author: ryan_
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+
+dataset = pd.read_csv("C:/Users/ryan_/Documents/Programming/repos/MLCourse/Machine Learning A-Z (Codes and Datasets)/Part 2 - Regression/Section 6 - Polynomial Regression/Python/Position_Salaries.csv")
+x = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
+
+lin_reg = LinearRegression()
+lin_reg.fit(x, y)
+
+poly_reg = PolynomialFeatures(degree = 2)
+x_poly = poly_reg.fit_transform(x)
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(x_poly, y)
+
+# plt.scatter(x, y, color='red')
+# plt.plot(x, lin_reg.predict(x), color='blue')
+# plt.title("Truth or Bluff (linear regression version)")
+# plt.xlabel("Position Level")
+# plt.ylabel("Salary")
+# plt.show()
+
+# plt.scatter(x, y, color='red')
+# plt.plot(x, lin_reg_2.predict(poly_reg.fit_transform(x)), color='purple')
+# plt.title("Truth or Bluff (polynomial regression version)")
+# plt.xlabel("Position Level")
+# plt.ylabel("Salary")
+# plt.show()
+
+plt.scatter(x, y, color='red', label='current salary points')
+plt.plot(x, lin_reg.predict(x), color='blue', label='predicted linear')
+plt.plot(x, lin_reg_2.predict(poly_reg.fit_transform(x)), color='purple', label='predicted polynomial')
+plt.title("Truth or Bluff (regression comparison version)")
+plt.xlabel("Position Level")
+plt.ylabel("Salary")
+plt.legend(loc='upper left')
+plt.show()
