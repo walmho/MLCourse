9389ba29f41cb465f6d561a7b48f816a02d8107d
Ryan Jones <jones.ryan.t.01@gmail.com>
2023-04-18 14:32:51 -0700

    DTR is not suited for 2D results

--- decisionTreeRegression.py
+++ decisionTreeRegression.py
@@ -4,23 +4,22 @@ import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
 
 #Importing data
-dataset = pd.read_csv('Position_Salaries.csv')
+dataset = pd.read_csv('C:/Users/ryan_/Documents/Programming/repos/MLCourse/Machine Learning A-Z (Codes and Datasets)/Part 2 - Regression/Section 8 - Decision Tree Regression/Python/Position_Salaries.csv')
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
 
-# Training the Decision Tree Regression model on the whole dataset
-
+#Training on whole dataset
 regressor = DecisionTreeRegressor(random_state = 0)
 regressor.fit(X, y)
-# Predicting a new result
 regressor.predict([[6.5]])
 
-# Visualising the Decision Tree Regression results (higher resolution)
-X_grid = np.arange(min(X), max(X), 0.01)
-X_grid = X_grid.reshape((len(X_grid), 1))
+#Visualize results
+# DTR IS NOT WELL SUITED FOR 2D RESULTS
+x_grid = np.arange(min(X), max(X), 0.01)
+x_grid = x_grid.reshape((len(x_grid), 1))
 plt.scatter(X, y, color = 'red')
-plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
-plt.title('Truth or Bluff (Decision Tree Regression)')
+plt.plot(x_grid, regressor.predict(x_grid), color = 'blue')
+plt.title('Salary vs. Experience (Decision Tree Regression version)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
